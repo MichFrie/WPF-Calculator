a2e5281ad53bcc3afcc1303d72ac0dc034bac90f
MichFrie <mifrpost@gmail.com>
2020-12-29 23:41:44 +0100

    Add project files.

---
+++ WPF-Calculator/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMenuExit (object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnMenuView(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnMenuAbout(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NumericButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }        
+        private void OperatingButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
